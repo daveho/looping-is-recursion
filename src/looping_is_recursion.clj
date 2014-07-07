e4b85133c2438873b789fd789378218afdb48d00
David Hovemeyer <david.hovemeyer@gmail.com>
2014-07-07 09:19:15 -0400

    implemented power, last-element, and seq= functions

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,13 +1,30 @@
 (ns looping-is-recursion)
 
+(defn power-help [base exp acc]
+  (if (= exp 0)
+    acc
+    (recur base (- exp 1) (* acc base))))
+
 (defn power [base exp]
-  ":(")
+  (power-help base exp 1))
+
+(defn last-element-help [a-seq]
+  (let [tail (rest a-seq)]
+    (if (empty? tail)
+      (first a-seq)
+      (recur tail))))
 
 (defn last-element [a-seq]
-  ":(")
+  (if (empty? a-seq)
+    nil
+    (last-element-help a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (cond
+    (empty? seq1) (empty? seq2)
+    (empty? seq2) false
+    (not (= (first seq1) (first seq2))) false
+    :else (recur (rest seq1) (rest seq2))))
 
 (defn find-first-index [pred a-seq]
   ":(")
